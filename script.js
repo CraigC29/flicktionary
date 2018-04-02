384b17a82ffdfa60111ed181723b7ab8ae722afb
Craig Cargill <craigcargill@craigs-mbp.home>
2018-04-02 23:50:25 +0100

    added functionality for series

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var pagePos = 3;
 var amountPages = 1;
 var allResults = "";
+var searchType = "movie";
 
 $(function(){
 
@@ -11,19 +12,35 @@ $(function(){
     getResultsFromTMDB(searchterms);
     return false;
   });
+
+  // var movieButton = document.getElementById('movieButton');
+  // movieButton.onClick = function(){
+  //   changeToMovie();
+  // };
+  //
+  // var seriesButton = document.getElementById('seriesButton');
+  // movieButton.onClick = function(){
+  //   changeToSeries();
+  // };
+
 });
 
 $(document).ready(function () {
 
     $("#submit").click(function (e) {
         closeSide();
+        mainBody.style.paddingTop = "100px";
 
 
         var validate = Validate();
 
         if (validate.length == 0) {
 
-        CallAPI(1);
+        if (searchType == "movie"){
+          CallAPI(1);
+        } else {
+          CallTVAPI(1);
+        }
 
         }
     });
@@ -82,6 +99,24 @@ $(document).ready(function () {
 
     }
 
+  // TV
+    function loadAllPagesTV(numOfPages){
+      var complete = false;
+      if (numOfPages > 10) {numOfPages = 10;}
+      for (i = 2; i <= numOfPages; i++){
+          console.log("Running on loop: " + i);
+          CallAPITVLoad(i);
+          if (i == numOfPages){
+            console.log("Complete = true");
+            complete = true;
+          }
+      }
+      if (complete = true){
+        printResults();
+      }
+
+    }
+
     function printResults(){
       console.log("Printing All Results");
       $("#message").html(allResults);
@@ -123,6 +158,81 @@ $(document).ready(function () {
         });
     }
 
+    function CallAPITVLoad(page) {
+        $.ajax({
+            url: "https://api.themoviedb.org/3/search/tv?&query=" + $("#searchInput").val() + "&page=" + page + "&include_adult=false",
+            // data: { "api_key": "58a54ae83bf16e590e2ef91a25247707" }, MY KEY REQUESTED TOO MANY TIMES
+            data: { "api_key": "c12b3a760b89eacb9d0da39c84baa696" },
+            dataType: "json",
+            success: function (result, status, xhr) {
+              if (page <= result["total_pages"]){
+                if (results["total_pages"] > 1){
+                  showLoadMore();
+                }
+                console.log("Total Pages: " + result["total_pages"])
+                for (i = 0; i < result["results"].length; i++) {
+                    var tvid = result["results"][i]["id"];
+                    var tvLocation = i;
+                    console.log(tvid);
+                    var image = result["results"][i]["poster_path"] == null ? "image unavailable sized.png" : "https://image.tmdb.org/t/p/w154/" + result["results"][i]["poster_path"];
+
+                    allResults.append("<div id=" + tvid + " class=\"result\" resourceId=\" titleText=\"" + result["results"][i]["title"] + "\">" + "<img onClick='openSide()' id=" + tvLocation + " class ='imageClick' src=\"" + image + "\"/>" + "</div>")
+                }
+
+                if (amountPages == page){
+                  console.log("All Pages");
+                  allResults.append("</div>");
+                }
+
+              } else {
+                hideLoadMore();
+              }
+            },
+            error: function (xhr, status, error) {
+                $("#message").html("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
+            }
+        });
+    }
+
+
+    // TV API
+    function CallTVAPI(page) {
+        $.ajax({
+            url: "https://api.themoviedb.org/3/search/tv?&query=" + $("#searchInput").val() + "&page=" + page,
+            // data: { "api_key": "58a54ae83bf16e590e2ef91a25247707" }, MY KEY REQUESTED TOO MANY TIMES
+            data: { "api_key": "c12b3a760b89eacb9d0da39c84baa696" },
+            dataType: "json",
+            success: function (result, status, xhr) {
+              pagePos = result["total_pages"];
+              amountPages = result["total_pages"];
+
+              console.log("Amount Of Pages Inside CallAPI = " + amountPages);
+              console.log("Total Pages According to actual API: " + result["total_pages"]);
+
+                // var allResults = $('<div class="resultDiv container">');
+                allResults = $('<div class="resultDiv container">');
+                  for (i = 0; i < result["results"].length; i++) {
+                      var tvid = result["results"][i]["id"];
+                      var tvLocation = i;
+                      console.log(tvid);
+                      var image = result["results"][i]["poster_path"] == null ? "image unavailable sized.png" : "https://image.tmdb.org/t/p/w154/" + result["results"][i]["poster_path"];
+
+                      allResults.append("<div id=" + tvid + " class=\"result\" resourceId=\" titleText=\"" + result["results"][i]["name"] + "\">" + "<img onClick='openSide()' id=" + tvLocation + " class ='imageClick' src=\"" + image + "\"/>" + "</div>")
+                  }
+
+                if (amountPages == 1){
+                  allResults.append("</div>");
+                  console.log("Pages = 1      Amount Pages: " + amountPages);
+                  printResults();
+                } else {
+                  loadAllPagesTV(amountPages);
+                }
+            },
+            error: function (xhr, status, error) {
+                $("#message").html("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
+            }
+        });
+    }
 
     function Validate() {
         var errorMessage = "";
@@ -153,6 +263,23 @@ function closeSide(){
   main.style.width = "100%";
 };
 
+function changeToMovie(){
+  searchType = "movie";
+  console.log("Movie");
+  document.getElementById('movieContainer').style.backgroundColor = "#2ee1e8";
+  document.getElementById('seriesContainer').style.backgroundColor = "transparent";
+  document.getElementById('mainBody').style.paddingTop = "170px";
+  $("#message").html("");
+}
+
+function changeToSeries(){
+  searchType = "series";
+  console.log("Series");
+  document.getElementById('seriesContainer').style.backgroundColor = "#2ee1e8";
+  document.getElementById('movieContainer').style.backgroundColor = "transparent";
+  document.getElementById('mainBody').style.paddingTop = "170px";
+  $("#message").html("");
+}
 
 function openSide(){
   console.log(event);
