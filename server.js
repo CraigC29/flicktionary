20c25cc518bf062681cb0d176436c92589b2cada
Craig Cargill <craigc29@btinternet.com>
2018-04-03 01:42:59 +0100

    renames

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.set('view engine', 'ejs');
 // use res.render to load up an ejs view file
 // index page
 app.get('/', function(req, res) {
- res.render('pages/index');
+ res.render('pages/flicktionary');
 });
 app.listen(8080);
 console.log('8080 is the magic port');
