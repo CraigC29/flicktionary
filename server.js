2cd03b683f144705c999b8ed08729131a1138b9e
BEN MARTIN (1604139) <1604139@rgu.ac.uk>
2018-04-17 16:36:15 +0100

    server stuff

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-app.get('/', function(req, res){
-  res.send("Hello world! by express");
+app.get('/flicktionary.html', function(req, res){
+ res.send("this is route 2");
 });
 app.listen(8080);
