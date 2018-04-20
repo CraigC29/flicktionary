6dfdc8fe45c0ff193b3e106650a6a5cee138e484
CRAIG CARGILL (1604113) <1604113@rgu.ac.uk>
2018-04-20 14:32:34 +0100

    changed to sessions

--- server.js
+++ server.js
@@ -98,11 +98,11 @@ app.get('/login', function(req, res) {
 app.get('/profile', function(req, res) {
   if(!req.session.loggedin){res.redirect('/login');return;}
   //get the requested user based on their username, eg /profile?username=dioreticllama
-  var uname = req.query.username;
+  // var uname = req.query.username;
   //this query finds the first document in the array with that username.
   //Because the username value sits in the login section of the user data we use login.username
   db.collection('people').findOne({
-    "login.username": uname
+    "login.username": req.session.user.login.username
   }, function(err, result) {
     if (err) throw err;
     //console.log(uname+ ":" + result);
