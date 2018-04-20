1436b1e213c1934eca542211d96c74be60868b91
Craig Cargill <craigc29@btinternet.com>
2018-04-21 00:38:10 +0100

    testing favourite

--- public/script.js
+++ public/script.js
@@ -102,7 +102,7 @@ $(document).ready(function () {
           var movieLocation = movieid;
           var image = result["results"][i]["poster_path"] == null ? "image unavailable sized.png" : "https://image.tmdb.org/t/p/w154/" + result["results"][i]["poster_path"];
 
-          allResults.append("<div id=" + movieid + " class=\"result\" resourceId=\" titleText=\"" + result["results"][i]["title"] + "\">" + '<% if (loggedIn) { %> <img class="facouriteIcon" src="/public/images/favoutite.png" onClick="addFavourite()" /> <% } %> ' + "<img id=" + movieLocation + " class ='imageClick' src=\"" + image + "\"/>" + "</div>")
+          allResults.append("<div id=" + movieid + " class=\"result\" resourceId=\" titleText=\"" + result["results"][i]["title"] + "\">" + '<% if (loggedIn) { %> <img class="facouriteIcon" src="/public/images/favourite.png" onClick="addFavourite()" /> <% } %> ' + "<img id=" + movieLocation + " class ='imageClick' src=\"" + image + "\"/>" + "</div>")
         }
 
         if (amountPages == 1){
@@ -116,17 +116,8 @@ $(document).ready(function () {
         $("#message").html("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
       }
     });
-    slowScroll();
   }
 
-  function slowScroll(){
-    window.setTimeout(scrollDown,100);
-    function scrollDown(){
-      $('html, body').animate({
-        scrollTop: $("#button").offset().top
-      }, 800);
-    }
-  }
 
   function loadGenre(genre){
       genreSelected = genre;
