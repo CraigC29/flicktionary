cfd3a97f891300d8b5e1fd75812c5529afa7a7d0
Craig Cargill <craigc29@btinternet.com>
2018-04-21 01:04:08 +0100

    changed position again

--- public/script.js
+++ public/script.js
@@ -102,7 +102,7 @@ $(document).ready(function () {
           var movieLocation = movieid;
           var image = result["results"][i]["poster_path"] == null ? "image unavailable sized.png" : "https://image.tmdb.org/t/p/w154/" + result["results"][i]["poster_path"];
 
-          allResults.append("<div id=" + movieid + " class=\"result\" resourceId=\" titleText=\"" + result["results"][i]["title"] + "\">" +  '<img class="favouriteIcon" src="/public/images/favourite.png" onClick="addFavourite()" />'  + "<img id=" + movieLocation + " class ='imageClick' src=\"" + image + "\"/>" + "</div>")
+          allResults.append("<div id=" + movieid + " class=\"result\" resourceId=\" titleText=\" moviePoster=\" + result["results"][i]["title"] + "\">" +  '<img class="favouriteIcon" src="/public/images/favourite.png" onClick="addFavourite()" />'  + "<img id=" + movieLocation + " class ='imageClick' src=\"" + image + "\"/>" + "</div>")
         }
 
         if (amountPages == 1){
