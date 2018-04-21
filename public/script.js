1afaa9141e7f7a962c98e7d59f41b8c1120ff7c6
Craig Cargill <craigc29@btinternet.com>
2018-04-21 04:59:19 +0100

    put within document ready

--- public/script.js
+++ public/script.js
@@ -53,28 +53,24 @@ $(document).ready(function () {
     loadHomepage();
   });
 
-
-
-  // $('.posterOverlay').on('click', function(e) {
-  //   console.log("registering click");
-  //   if (e.target == this){
-  //     console.log("working");
-  //     if(searchType == "movie"){
-  //       var path = $(this).attr('id');
-  //       var movieid = path;
-  //       var urlMediaMovies = "/mediaMovies?id=" + movieid;
-  //       window.location.replace(urlMediaMovies);
-  //     } else {
-  //       var path = $(this).attr('id');
-  //       var tvid = path;
-  //       var urlMediaSeries = "/mediaSeries?id=" + tvid;
-  //       window.location.replace(urlMediaSeries);
-  //     }
-  //   } else{
-  //     console.log("not working");
-  //   }
-  // });
-
+  $(document).ready(function(){
+    $(".posterOverlay").on('click', function(e) {
+      if (e.target !== this){
+        console.log("working");
+        if(searchType == "movie"){
+          var path = $(this).attr('id');
+          var movieid = path;
+          var urlMediaMovies = "/mediaMovies?id=" + movieid;
+          window.location.replace(urlMediaMovies);
+        } else {
+          var path = $(this).attr('id');
+          var tvid = path;
+          var urlMediaSeries = "/mediaSeries?id=" + tvid;
+          window.location.replace(urlMediaSeries);
+        }
+      }
+    });
+  });
   $("#searchInput").keyup(function() {
     if (event.keyCode === 13) {
       document.getElementById("submit").click();
@@ -115,7 +111,7 @@ $(document).ready(function () {
           var movieLocation = movieid;
           var image = result["results"][i]["poster_path"] == null ? "image unavailable sized.png" : "https://image.tmdb.org/t/p/w154/" + result["results"][i]["poster_path"];
 
-          allResults.append("<div id=" + movieid + " class=\"result\" resourceId=\" titleText=\"" + result["results"][i]["title"] + "\">" + "<div class='imageOverlayPoster'> <div class='posterOverlay' id=" + movieLocation + ">" + '<img class="favouriteIcon" src="/public/images/favourite.png"  />' + "</div>" + "<img id=" + movieLocation + " class ='imageClick' src=\"" + image + "\"/>"  + "</div></div>")
+          allResults.append("<div id=" + movieid + " class=\"result\" resourceId=\" titleText=\"" + result["results"][i]["title"] + "\">" + "<div class='imageOverlayPoster'> <div class='posterOverlay' id=" + movieLocation + ">" + '<img class="favouriteIcon" src="/public/images/favourite.png" onClick="addFavourite()" />' + "</div>" + "<img id=" + movieLocation + " class ='imageClick' src=\"" + image + "\"/>"  + "</div></div>")
         }
 
         if (amountPages == 1){
@@ -179,7 +175,7 @@ $(document).ready(function () {
             var movieid = result["results"][i]["id"];
             var movieLocation = movieid;
             var image = result["results"][i]["poster_path"] == null ? "image unavailable sized.png" : "https://image.tmdb.org/t/p/w154/" + result["results"][i]["poster_path"];
-            allResults.append("<div id=" + movieid + "  class=\"result\" resourceId=\" titleText=\"" + result["results"][i]["title"] + "\">" + "<div class='imageOverlayPoster'> <div class='posterOverlay' id=" + movieLocation + ">" + '<img class="favouriteIcon"  src="/public/images/favourite.png"  />' + "</div>" + "<img id=" + movieLocation + " class ='imageClick' src=\"" + image + "\"/>"  + "</div></div>")
+            allResults.append("<div id=" + movieid + "  class=\"result\" resourceId=\" titleText=\"" + result["results"][i]["title"] + "\">" + "<div class='imageOverlayPoster'> <div class='posterOverlay' id=" + movieLocation + ">" + '<img class="favouriteIcon"  src="/public/images/favourite.png" onClick="addFavourite()" />' + "</div>" + "<img id=" + movieLocation + " class ='imageClick' src=\"" + image + "\"/>"  + "</div></div>")
           }
           if (amountPages == page){
             allResults.append("</div>");
@@ -522,7 +518,7 @@ function CallAPILoadPopularMedia(media, page) {
         var movieLocation = movieid;
         var image = result["results"][i]["poster_path"] == null ? "image unavailable sized.png" : "https://image.tmdb.org/t/p/w154/" + result["results"][i]["poster_path"];
 
-        allResults.append("<div id=" + movieid + "  class=\"result\" resourceId=\" titleText=\"" + result["results"][i]["title"] + "\">" + "<div class='imageOverlayPoster'> <div class='posterOverlay' id=" + movieLocation + ">" + '<img class="favouriteIcon" src="/public/images/favourite.png"  />' + "</div>" + "<img id=" + movieLocation + " class ='imageClick' src=\"" + image + "\"/>"  + "</div></div>");
+        allResults.append("<div id=" + movieid + "  class=\"result\" resourceId=\" titleText=\"" + result["results"][i]["title"] + "\">" + "<div class='imageOverlayPoster'> <div class='posterOverlay' id=" + movieLocation + ">" + '<img class="favouriteIcon" src="/public/images/favourite.png" onClick="addFavourite()" />' + "</div>" + "<img id=" + movieLocation + " class ='imageClick' src=\"" + image + "\"/>"  + "</div></div>");
       }
       if (page == 10){
         allResults.append("</div>");
@@ -563,10 +559,6 @@ $(document).on('click', ".episodeBlock", function() {
   episodeSelected = episode;
   loadIndividualEpisodeData();
 
-});
-
-$('.posterOverlay').on('click', function() {
-console.log("registering click");
 });
 
 
