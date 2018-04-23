71b72bbdae52b4cba55837ab52d9f94043a802a5
CRAIG CARGILL (1604113) <1604113@rgu.ac.uk>
2018-04-23 13:06:14 +0100

    jquery servserSide

--- server.js
+++ server.js
@@ -145,7 +145,8 @@ app.post('/favourite', function (req, res){
   // console.log(JSON.stringify(req.params))
   var userId = req.session.user._id
   // var medId = req.params.id
-  var medId = req.Form['mediaId'];
+  console.log($(this).parent().attr('id'));
+  // var medId = req.Form['mediaId'];
   console.log("User Id: " + userId);
   console.log("mediaId: " + medId);
   db.collection('people').update(
