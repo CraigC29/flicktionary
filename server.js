2ec48553b2611a5be8c5de8bad9b4ee3c036caab
Craig Cargill <craigc29@btinternet.com>
2018-04-24 17:41:18 +0100

    check if logged in

--- server.js
+++ server.js
@@ -143,13 +143,15 @@ app.get('/adduser', function(req, res) {
 
   app.post('/favourite', function (req, res){
 
-    if(!req.session.loggedin){return false;}
+    if(req.session.loggedin){
 
-    db.collection('people').findOne({"login.username":req.session.user.login.username}, function(err, result) {
-      db.collection('people').update({"_id":result._id}, {$push:{"favourites" : {"place" : {"mediaId":req.body.favMed}}}});
-      console.log("Added Media: " + req.body.favMed);
-    });
-    res.redirect('/profile');
+      db.collection('people').findOne({"login.username":req.session.user.login.username}, function(err, result) {
+        db.collection('people').update({"_id":result._id}, {$push:{"favourites" : {"place" : {"mediaId":req.body.favMed}}}});
+        console.log("Added Media: " + req.body.favMed);
+      });
+      res.redirect('/profile');
+
+    }
     // console.log(JSON.stringify(req.params))
     // var userId = req.session.user._id
     // var medId = req.params.id
